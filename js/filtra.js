db1de30b8a1002f64f6cdfaf9c39b3e620b228c0
Alice <alicedealmeida379@gmail.com>
2022-10-17 13:38:23 -0300

    Finalização do filtro

--- js/filtra.js
+++ js/filtra.js
@@ -3,19 +3,16 @@ var filtro = document.querySelector('#filtrar-tabela');
 filtro.addEventListener("input", function(){
     var pacienteNomes = document.querySelectorAll('.info-nome');
     var textoDigitado = filtro.value;
-    console.log(textoDigitado);
-    
-    // for (i=0; i<pacienteNomes.length; i++){
-    //     console.log(pacienteNomes[i].textContent);
-    // }
 
-    pacienteNomes.forEach(function(paciente){
-        nome = paciente.textContent;
-
-        if (nome == textoDigitado) {
-            console.log("são iguais");
-            paciente.parentNode.style.backgroundColor = "blue";
-        }
-    });
-
+    if(textoDigitado.length > 0){
+        pacienteNomes.forEach(function(paciente){
+            nome = paciente.textContent; 
+            if (nome != textoDigitado) {
+                paciente.parentNode.classList.add("invisivel");
+            } else {
+                paciente.parentNode.classList.remove("invisivel");
+            }
+        });
+    }
 });
+
