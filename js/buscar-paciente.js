5d304f0dd4c50c06507a35b0903b2332c861acb8
Alice <alicedealmeida379@gmail.com>
2022-10-17 20:18:11 -0300

    Lidando com erros da requisição  AJAX

--- js/buscar-paciente.js
+++ js/buscar-paciente.js
@@ -1,7 +1,8 @@
 var botaoBuscar = document.querySelector("#buscar-pacientes");
 
 botaoBuscar.addEventListener("click", function() {
-    var xhr = new XMLHttpRequest;
+
+    var xhr = new XMLHttpRequest();
     xhr.open("GET", "https://api-pacientes.herokuapp.com/pacientes");
     
     //Para pegarmos a resposta quando a requisiçao HTTP voltar 
@@ -9,19 +10,25 @@ botaoBuscar.addEventListener("click", function() {
     //escutando o evento de load:
     xhr.addEventListener("load", function(){
         console.log(xhr.responseText);
+        var erroAjax = document.querySelector("#erro-ajax");
         /* Se você observar o que é impresso no console, 
         você vai ver que o servidor nos retorna um JSON, 
         um formato de texto muito comum hoje em dia na web.
         Como não queremos trabalhar com texto e sim com objetos Javascript, 
         vamos parsear este texto para um objeto Javascript equivalente. 
         Use a função JSON.parse() na resposta para transformar:*/
-
-        var resposta = xhr.responseText;
-        var pacientes = JSON.parse(resposta); // array de pacientes
-
-        pacientes.forEach(function(paciente) {
-            adicionaPaciente(paciente);
-        });
+ 
+        if (xhr.status == 200) {
+            erroAjax.classList.add("invisivel");
+            var resposta = xhr.responseText;
+            var pacientes = JSON.parse(resposta); // array de pacientes
+            
+            pacientes.forEach(function(paciente) {
+                adicionaPaciente(paciente);
+            });
+        } else {
+            erroAjax.classList.remove("invisivel");
+        }
     });
 
     //envio da requisição
